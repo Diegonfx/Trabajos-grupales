3bf9a4fe7387446a35170799d13bae05d454f9db
DiegoMancini <diegonfx@gmail.com>
2017-03-10 19:08:53 -0300

    TP1 hecho con algoritmos de internet

---
+++ src/TrabajoPractico1/Sort/SortInternet.java
@@ -0,0 +1,90 @@
+package TrabajoPractico1.Sort;
+
+/**
+ * Created by DiegoMancini on 7/3/17.
+ */
+public class SortInternet {
+
+    public void bubbleSort(int[] arr) {
+        boolean swapped = true;
+        int j = 0;
+        int tmp;
+        while (swapped) {
+            swapped = false;
+            j++;
+            for (int i = 0; i < arr.length - j; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    tmp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = tmp;
+                    swapped = true;
+                }
+            }
+        }
+    }
+
+    public void selectionSort(int[] arr) {
+        int i;
+        int j;
+        int minIndex;
+        int tmp;
+        int n = arr.length;
+        for (i = 0; i < n - 1; i++) {
+            minIndex = i;
+            for (j = i + 1; j < n; j++)
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            if (minIndex != i) {
+                tmp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = tmp;
+            }
+        }
+    }
+
+    public void insertionSort(int[] arr) {
+        int i, j, newValue;
+        for (i = 1; i < arr.length; i++) {
+            newValue = arr[i];
+            j = i;
+            while (j > 0 && arr[j - 1] > newValue) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = newValue;
+        }
+    }
+
+    public int partition(int[] arr, int left, int right) {
+        int i = left, j = right;
+        int tmp;
+        int pivot = arr[(left + right) / 2];
+
+        while (i <= j) {
+            while (arr[i] < pivot)
+                i++;
+            while (arr[j] > pivot)
+                j--;
+            if (i <= j) {
+                tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+                i++;
+                j--;
+            }
+        }
+
+        return i;
+    }
+    public void quickSort(int[] arr, int left, int right) {
+        int index = partition(arr, left, right);
+        if (left < index - 1)
+            quickSort(arr, left, index - 1);
+        if (index < right)
+            quickSort(arr, index, right);
+    }
+
+
+
+
+}
